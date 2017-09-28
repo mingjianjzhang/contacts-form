d8537d5f8d63bc6a0d629fcadecf7cb35374ef50
Josh Zhang <josh.zhang91@gmail.com>
2017-09-27 20:42:16 -0700

    finished form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import us from 'us';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Contact Formddddd';
